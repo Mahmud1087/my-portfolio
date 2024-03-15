86d335d033c20624c11ad6356d54cdf665257f78
Mahmud1087 <abdulazeezm578@gmail.com>
2024-03-15 16:23:19 +0100

    added framer animation to the About page

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,10 +1,22 @@
+import { motion } from 'framer-motion';
+
 const Heading = ({ title }) => {
   return (
-    <h1 className='text-white font-jolly text-[5rem]'>
+    <motion.h1
+      initial={{ x: -500 }}
+      animate={{ x: 0 }}
+      transition={{
+        type: 'linear',
+        duration: 0.7,
+        delay: 0.4,
+        ease: [0.2, 0.71, 0.2, 1.01],
+      }}
+      className='text-white font-jolly text-[5rem]'
+    >
       <span className='code'>{`< `}</span>
       {title}
       <span className='code'>{` />`}</span>
-    </h1>
+    </motion.h1>
   );
 };
 export default Heading;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,27 +1,39 @@
-import { Link } from 'react-router-dom';
 import Heading from '../components/Heading';
 import Button from '../components/Button';
+import { motion } from 'framer-motion';
 
 const About = () => {
   return (
-    <div className='w-[50%] h-screen bg-[#1e1e1e] flex flex-col gap-8 p-16'>
+    <div className='w-[50%] h-screen bg-[#1e1e1e] flex flex-col gap-8 p-16 overflow-hidden'>
       <Heading title='about me' />
-      <p className='text-[#A79F9F] font-lexend leading-8'>
-        Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut.
-        Carrot cake caramels pie sweet apple pie tiramisu carrot cake. Marzipan
-        marshmallow croissant tootsie roll lollipop. Cupcake lemon drops bear
-        claw gummies. Jelly bear claw gummi bears lollipop cotton candy gummi
-        bears chocolate bar cake cookie. Cupcake muffin danish muffin cookie
-        gummies.
-      </p>
-      <p className='text-[#A79F9F] font-lexend leading-8'>
-        Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut.
-        Carrot cake caramels pie sweet apple pie tiramisu carrot cake. Marzipan
-        marshmallow croissant tootsie roll lollipop. Cupcake lemon drops bear
-        claw gummies. Jelly bear claw gummi bears lollipop cotton candy gummi
-        bears chocolate bar cake cookie. Cupcake muffin danish muffin cookie
-        gummies.
-      </p>
+      <motion.div
+        initial={{ y: -1000 }}
+        animate={{ y: 0 }}
+        transition={{
+          type: 'linear',
+          duration: 0.7,
+          delay: 0.8,
+          ease: [0.2, 0.71, 0.2, 1.01],
+        }}
+        className='flex flex-col gap-8 '
+      >
+        <p className='text-[#A79F9F] font-lexend leading-8'>
+          Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut.
+          Carrot cake caramels pie sweet apple pie tiramisu carrot cake.
+          Marzipan marshmallow croissant tootsie roll lollipop. Cupcake lemon
+          drops bear claw gummies. Jelly bear claw gummi bears lollipop cotton
+          candy gummi bears chocolate bar cake cookie. Cupcake muffin danish
+          muffin cookie gummies.
+        </p>
+        <p className='text-[#A79F9F] font-lexend leading-8 mb-5'>
+          Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut.
+          Carrot cake caramels pie sweet apple pie tiramisu carrot cake.
+          Marzipan marshmallow croissant tootsie roll lollipop. Cupcake lemon
+          drops bear claw gummies. Jelly bear claw gummi bears lollipop cotton
+          candy gummi bears chocolate bar cake cookie. Cupcake muffin danish
+          muffin cookie gummies.
+        </p>
+      </motion.div>
       <Button link='/contact' title='Contact Me' />
     </div>
   );
