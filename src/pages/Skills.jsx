ea629a3f14c711377633ac37420e8ccaa8dcf9ee
Mahmud1087 <abdulazeezm578@gmail.com>
2024-03-15 17:23:23 +0100

    added framer animation to the Skills page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,24 +1,48 @@
 import Heading from '../components/Heading';
 import ProgressBar from '../components/ProgressBar';
 import { skills } from '../data';
+import { motion, stagger } from 'framer-motion';
 
 const Skills = () => {
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.5,
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
+  const items = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
   return (
     <div className='de w-[50%] h-screen bg-[#1e1e1e] p-16 overflow-scroll'>
       <Heading title='skills' />
-      <section className='h-full w-full grid grid-cols-3 gap-5 mt-8'>
+      <motion.section
+        variants={container}
+        initial='hidden'
+        animate='visible'
+        className='h-full w-full grid grid-cols-3 gap-5 mt-8'
+      >
         {skills.map((item) => {
           const { id, bg, skill } = item;
           return (
-            <div
-              key={id}
-              className='rounded-xl bg-[#0000003d] flex items-center justify-center shadow-sm shadow-gray-600 hover:scale-[1.1] transition-all ease-in-out'
-            >
-              <ProgressBar bg={bg} skill={skill} />
-            </div>
+            <motion.div key={id} variants={items}>
+              <div className='p-8 rounded-xl bg-[#0000003d] flex items-center justify-center shadow-sm shadow-gray-600 hover:scale-[1.1] transition-all ease-in-out'>
+                <ProgressBar bg={bg} skill={skill} />
+              </div>
+            </motion.div>
           );
         })}
-      </section>
+      </motion.section>
     </div>
   );
 };
