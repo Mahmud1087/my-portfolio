e05c8a4e458e6add51d0ffa11a5e06b3bd059058
Mahmud1087 <abdulazeezm578@gmail.com>
2024-03-16 20:04:43 +0100

    Added validation to the form, also added react-toastify for submission of the form

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -2,8 +2,29 @@ import { useState } from 'react';
 import Heading from '../components/Heading';
 import { motion } from 'framer-motion';
 import { sendCustomEmail } from '../components/emailHelper';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ContactMe = () => {
+  const msgSent = () =>
+    toast.success('Message sent, will get back to you soon', {
+      position: 'top-center',
+      closeOnClick: true,
+      // className: 'sent',
+    });
+
+  const msgEmpty = () =>
+    toast.error('One or more fields is empty', {
+      position: 'top-center',
+      closeOnClick: true,
+    });
+
+  const validate = () => {
+    details.name === '' || details.email === '' || details.message === ''
+      ? msgEmpty()
+      : msgSent();
+  };
+
   const initialDetailsState = {
     name: '',
     email: '',
@@ -26,6 +47,7 @@ const ContactMe = () => {
 
   return (
     <div className='w-full h-screen bg-[#1e1e1e] p-16 overflow-scroll lg:w-1/2'>
+      <ToastContainer />
       <Heading title='contact me' />
       <motion.form
         initial={{ y: 1000 }}
@@ -48,6 +70,7 @@ const ContactMe = () => {
             name='name'
             value={details.name}
             onChange={handleDetailsChange}
+            required
           />
         </label>
         <label htmlFor='email' className='lbl'>
@@ -59,6 +82,7 @@ const ContactMe = () => {
             name='email'
             value={details.email}
             onChange={handleDetailsChange}
+            required
           />
         </label>
         <label htmlFor='message' className='lbl'>
@@ -72,11 +96,13 @@ const ContactMe = () => {
             rows='10'
             value={details.message}
             onChange={handleDetailsChange}
+            required
           ></textarea>
         </label>
         <button
           type='submit'
           className='border border-[#21ff60ad] bg-[#21ff60ad] hover:bg-transparent transition-all ease-in-out font-jolly text-4xl w-fit py-3 px-14 text-white rounded-lg lg:text-2xl lg:py-2 lg:px-10'
+          onClick={validate}
         >
           Send
         </button>
