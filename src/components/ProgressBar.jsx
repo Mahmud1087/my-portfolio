d8e7dcf3583c8613bab55511ae2cab21d32a69de
Mahmud1087 <abdulazeezm578@gmail.com>
2024-03-17 19:36:29 +0100

    Made the skills page mobile responsive

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,10 +1,12 @@
 const ProgressBar = ({ skill, bg }) => {
   return (
     <div
-      className={`h-[150px] w-[150px] ${bg} rounded-full flex items-center justify-center lg:h-[120px] lg:w-[120px]`}
+      className={`h-[110px] w-[110px] sm:h-[150px] sm:w-[150px] ${bg} rounded-full flex items-center justify-center lg:h-[120px] lg:w-[120px]`}
     >
-      <div className='w-[130px] h-[130px] bg-gray-300 border border-[#1e1e1e] rounded-full flex items-center justify-center lg:h-[100px] lg:w-[100px]'>
-        <h1 className='font-lexend text-xl font-bold lg:text-base'>{skill}</h1>
+      <div className='h-[95px] w-[95px] sm:w-[130px] sm:h-[130px] bg-gray-300 border border-[#1e1e1e] rounded-full flex items-center justify-center lg:h-[100px] lg:w-[100px]'>
+        <h1 className='font-lexend font-bold text-base sm:text-xl lg:text-base'>
+          {skill}
+        </h1>
       </div>
     </div>
   );
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -6,19 +6,19 @@ import { container, items } from '../components/utils';
 
 const Skills = () => {
   return (
-    <div className='w-full h-screen bg-[#1e1e1e] p-16 overflow-scroll lg:w-1/2'>
+    <div className='w-full h-screen bg-[#1e1e1e] p-5 overflow-scroll pb-16 sm:pb-0 sm:p-16 lg:w-1/2'>
       <Heading title='skills' />
       <motion.section
         variants={container}
         initial='hidden'
         animate='visible'
-        className='h-full w-full grid grid-cols-2 gap-5 mt-8 lg:grid-cols-3'
+        className='h-full w-full grid grid-cols-2 mt-8 gap-3 sm:gap-5 lg:grid-cols-3'
       >
         {skills.map((item) => {
           const { id, bg, skill } = item;
           return (
             <motion.div key={id} variants={items}>
-              <div className='p-12 rounded-xl bg-[#0000003d] flex items-center justify-center shadow-sm shadow-gray-600 hover:scale-[1.1] transition-all ease-in-out lg:p-8'>
+              <div className='py-10 rounded-xl bg-[#0000003d] flex items-center justify-center shadow-sm shadow-gray-600 hover:scale-[1.1] transition-all ease-in-out sm:p-12 lg:p-8'>
                 <ProgressBar bg={bg} skill={skill} />
               </div>
             </motion.div>
