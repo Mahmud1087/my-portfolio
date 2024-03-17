1fdf5a0ed78346d8f48e442b158b4c65c83124b7
Mahmud1087 <abdulazeezm578@gmail.com>
2024-03-17 15:44:56 +0100

    Added the navbar for mobile version

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import SideNav from './components/SideNav';
 import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 
 const Layout = () => {
   return (
-    <div className='flex'>
+    <div className='sm:flex'>
       <SideNav />
+      <Navbar />
       <Hero />
       <Outlet />
     </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { FaBars } from 'react-icons/fa6';
+import logo from '../assets/e-icon.png';
+
+const Navbar = () => {
+  return (
+    <div className='w-full flex items-center justify-between p-5 border-b-2 border-b-[#00000088] fixed z-10 sm:hidden'>
+      <div className='px-2 py-1 rounded-sm bg-[#21ff60ad]'>
+        <img src={logo} alt='Developer logo' className='w-5' />
+      </div>
+      <button className='outline-none text-[#fff] text-xl'>
+        <FaBars />
+      </button>
+    </div>
+  );
+};
+export default Navbar;
